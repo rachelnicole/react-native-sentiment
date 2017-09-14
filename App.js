fa23ab9ebef245a21a17bd412bdb2c34c0326506
Rachel White <loveless@gmail.com>
2017-09-14 14:22:35 -0400

    getting base styling done of the UI

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Container, Header, Content, Footer, Title} from 'native-base';
+import React, {Component} from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default class App extends React.Component {
+
+export default class LayoutExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      return (
+          <Container style={styles.main}>
+
+              <Content>
+              <Text style={styles.title}>Sentiment</Text>
+              <View style={styles.textBorderStyle}>
+              <TextInput
+                style={styles.textarea}
+                placeholder="Add text here..."
+                onChangeText={(text) => this.setState({text})}
+              />
+              </View>
+              </Content>
+
+          </Container>
+      );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  main: {
+    backgroundColor: '#FF6F69',
+  },
+  title: {
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+    padding: 50,
+    fontFamily: 'Courier',
+  },
+  textBorderStyle: {
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#ccc',
+    borderRadius: 10, 
+    height: 40,
+    width: 300,
+    backgroundColor: 'white',
+  },
+  textarea: {
+    fontFamily: 'Courier',
+    fontSize: 15,
+    padding: 20,
   },
 });
